fb515da0df1e79789db17ad217e460ab5bd389cb
Pedro Miola <pedromiola2@gmail.com>
2022-04-28 11:25:43 -0300

    Add Função de Comp

--- TAD LSE/LSE.c
+++ TAD LSE/LSE.c
@@ -122,3 +122,108 @@ void imprimeLista (LSE *list)
         i++;
     }
 }
+
+LSE *insereInicioComp (LSE *list, int num, int *comp)
+{
+    LSE *novo;
+    novo = (LSE*) malloc (sizeof(LSE));
+    novo->num = num;
+
+    novo->prox = list;
+    list = novo;
+    return list;
+}
+
+LSE *insereFinalComp (LSE *list, int num, int *comp)
+{
+    LSE *novo, *aux;
+    novo = (LSE*) malloc (sizeof(LSE));
+    novo->num = num;
+    novo->prox = NULL;
+
+    *comp += 1;
+    if (list == NULL)
+    {
+        
+        list = novo;
+        return list;
+    }
+
+    aux = list;
+    while (aux->prox != NULL)
+    {
+        aux = aux->prox;
+        *comp += 1;
+    }
+    aux->prox = novo;
+    return list;
+}
+
+LSE *insereMeioComp (LSE *list, int num, int *comp)
+{
+    int tamanhoLista = 0;
+    LSE *novo, *aux; 
+    novo = (LSE*) malloc (sizeof(LSE));
+    novo->num = num;
+
+    *comp += 1;
+    if (list == NULL)
+    {
+        novo->prox = NULL;
+        list = novo;
+        return list;
+    }
+
+    aux = list;
+    while (aux != NULL)
+    {
+        aux = aux->prox;
+        tamanhoLista++;
+        *comp += 1;
+    }
+
+    tamanhoLista = tamanhoLista/2;
+    tamanhoLista--;
+    aux = list;
+
+    while(tamanhoLista > 0)
+    {
+        aux = aux->prox;
+        tamanhoLista--;
+        *comp += 1;
+    }
+
+    novo->prox = aux->prox;
+    aux->prox = novo;
+    return list;
+}
+
+void consultaNumComp (LSE *list, int num, int *comp)
+{
+    LSE *aux; 
+    aux = list;
+    int i = 0;
+    while (aux != NULL)
+    {
+        if (i == 0)
+        {
+            *comp += 1;
+            *comp += 1;
+
+        }
+
+        if(aux->num == num)
+        {
+            i++;
+        }            
+        aux = aux->prox;
+    }
+    if(i == 0)
+    {
+        printf("Numero nao presente");
+    }
+    else
+    {
+        printf("Numero encontrado");
+    }
+}
--- TAD LSE/LSE.h
+++ TAD LSE/LSE.h
@@ -5,9 +5,16 @@ typedef struct LSE
 }LSE;
 
 LSE *criaLista (void);
+
 LSE *insereInicio (LSE *list, int num);
 LSE *insereFinal (LSE *list, int num);
 LSE *insereMeio (LSE *list, int num);
+
+LSE *insereInicioComp (LSE *list, int num, int *comp);
+LSE *insereFinalComp (LSE *list, int num, int *comp);
+LSE *insereMeioComp (LSE *list, int num, int *comp);
+
 LSE *destroiLista (LSE *list);
 void consultaNum (LSE *list, int num);
+void consultaNumComp (LSE *list, int num, int *comp);
 void imprimeLista (LSE *list);
